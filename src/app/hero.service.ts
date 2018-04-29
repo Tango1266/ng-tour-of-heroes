891a5d78290519ac45c65202207be5ebd9d0eb0b
Daniele Plantera <tango1266@arcor.de>
2018-04-29 10:45:32 +0200

    Added getHeroes method to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes'
 
 @Injectable()
 export class HeroService {
@@ -6,4 +8,7 @@ export class HeroService {
     constructor() {
     }
 
+    getHeroes(): Hero[] {
+        return HEROES;
+    }
 }
