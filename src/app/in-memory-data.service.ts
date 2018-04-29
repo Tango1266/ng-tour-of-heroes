55c502bc1399c417049f423d1082ff6c389720c0
Daniele Plantera <tango1266@arcor.de>
2018-04-29 16:57:06 +0200

    Fixed when data wasn't provided

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -15,6 +15,6 @@ export class InMemoryDataService implements InMemoryDbService {
             {id: 19, name: 'Magma'},
             {id: 20, name: 'Tornado'}
         ];
-        return heroes;
+        return {heroes};
     }
 }
