357ace0528c1c6b1d6bb384aa622ca9dc0b9ef56
Daniele Plantera <tango1266@arcor.de>
2018-04-29 16:18:36 +0200

    Add HeroService.getHero()

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,6 @@ import {HEROES} from './mock-heroes'
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of'
 import {MessageService} from "./message.service";
-import {elementClass} from "@angular/core/src/render3/instructions";
 
 @Injectable()
 export class HeroService {
@@ -18,5 +17,10 @@ export class HeroService {
 
         /*rxjs.observable.of() creates a Observable of HEROES*/
         return of(HEROES);
+    }
+
+    getHero(id: number): Observable<Hero> {
+        this.messageService.add(`HeroService: fetching hero id =${id}`);
+        return of(HEROES.find(_hero => _hero.id == id));
     }
 }
