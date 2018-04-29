6d82f7cea3827e30f3915f8aba7f9798bd4ed73f
Daniele Plantera <tango1266@arcor.de>
2018-04-29 11:12:02 +0200

    Wired MessageService into HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,18 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes'
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of'
+import {MessageService} from "./message.service";
+import {elementClass} from "@angular/core/src/render3/instructions";
 
 @Injectable()
 export class HeroService {
 
-    constructor() {
+    constructor(private messageService: MessageService) {
     }
 
     getHeroes(): Observable<Hero[]> {
+
+        this.messageService.add('HeroService: fetching heroes...');
 
         /*rxjs.observable.of() creates a Observable of HEROES*/
         return of(HEROES);
